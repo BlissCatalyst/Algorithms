4ff0f6cfd23bda8dcb7ec9d9a21a085965d6383e
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-07-18 13:25:36 -0600

    finished recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,24 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    required_ents = recipe.keys()
+    lowest_batches_possible = None
+
+    for ingredient in required_ents:
+        batches_possible = ingredients.get(
+            ingredient, 0) // recipe.get(ingredient)
+        if lowest_batches_possible is None or batches_possible < lowest_batches_possible:
+            lowest_batches_possible = batches_possible
+
+    return lowest_batches_possible
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
